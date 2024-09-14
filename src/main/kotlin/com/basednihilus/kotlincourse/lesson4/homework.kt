ae51694649a9f2cc0d67f6c3e485492fce2703a1
BasedNihilus <davletmg@gmail.com>
2024-09-15 01:05:08 +0300

    initial commit

---
+++ src/main/kotlin/com/basednihilus/kotlincourse/lesson4/homework.kt
@@ -0,0 +1,42 @@
+package com.basednihilus.kotlincourse.lesson4
+
+42 Byte
+98765432123456789L Long
+23.45f Float
+0.123456789 Double
+"Kotlin & Java" String
+false Boolean
+'c' String
+500 Short
+4294967296L Long
+18.0f Float
+-0.001 Double
+"OpenAI" String
+“true” Boolean
+'9' String
+2048 Short
+10000000000L Long
+5.75f Float
+1.414 Double
+"Artificial Intelligence" String
+false Boolean
+'@' String
+1024 Short
+1234567890123L Long
+10.01f Float
+-273.15 Double
+"SpaceX" String
+true Boolean
+“🤯” String
+‘65535’ String
+72057594037927935L Long
+2.71828f Float
+101.0101 Double
+"Quantum Computing" String
+false Boolean
+'x' String
+314  Short
+123456789123456789L Long
+6.626f Float
+0.007 Double
+"Android Studio" String
