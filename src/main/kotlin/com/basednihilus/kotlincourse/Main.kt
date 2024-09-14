59efa944841a2dccf700191d3f32bdaf2f0220df
BasedNihilus <davletmg@gmail.com>
2024-09-15 00:51:06 +0300

    initial commit

---
+++ src/main/kotlin/com/basednihilus/kotlincourse/Main.kt
@@ -0,0 +1,16 @@
+package com.basednihilus.com.basednihilus.kotlincourse
+
+val VIN: String = "abcd1234"
+
+var colour: String = "Red"
+
+var distance: Double = 0.0
+
+lateinit var owner: String
+
+const val WHEELS: Int = 4
+
+val report: String by lazy {
+    ""
+}
+
---
+++ src/main/kotlin/com/basednihilus/kotlincourse/lesson3/homework.kt
@@ -0,0 +1,111 @@
+package com.basednihilus.kotlincourse.lesson3
+
+import kotlin.random.Random
+
+val EventName: String = "Hackathon Survival"
+
+var EventDate: String = "09.09.2024"
+
+val EventPlace: String = "Kazan, RT"
+
+val GearBudget: Int = 250000
+
+val CateringBudget: Int = 250000
+
+val OtherBudget: Int = 500000
+
+val TotalEventBudget: Int = GearBudget + CateringBudget + OtherBudget
+
+val Members: Int = 100
+
+val Duration: String = "10 days"
+
+val ContactInfo: String = "davletmg@gmail.com"
+
+val Terms: String = "Basic terms"
+
+val EventStatusReady: Boolean = true
+
+val SponsorList: String = "Nvidia, AMD"
+
+val EventBudget: Int = TotalEventBudget
+
+var InternetStatusReady: Boolean = true
+
+var TransportInfo: String = "buses"
+
+var Resourses: String = "all inclusive"
+
+var Coordination: String = "full support"
+
+val Teams: Int = 10
+
+val TasksList: String = "create and run software"
+
+var EscapePlan: String = "ready"
+
+val AvailableGearList: String = "PCs and laptops"
+
+val FreeGearList: String = "PCs"
+
+var MealTime: String = "every 6 hours"
+
+var ErrorPlan: String = "ready"
+
+var JuryList: String = "members of" + SponsorList
+
+var Methods: String = "interview"
+
+var RoomTemperature: Int = 20
+
+var NetworkMonitoring: Boolean = true
+
+var LightLevel: Int = 100
+
+var EventLog: String = "event start, event end"
+
+var MedicalHelp: Boolean = false
+
+var SecurityPlan: String = ErrorPlan + EscapePlan
+
+val RegistrationNumber: Int = 12345678
+
+val MaximumNoise: Int = 100
+
+var NoiseIndication: Boolean = false
+
+val EventType: String = "bootcamp"
+
+var FreeSitSpace: Int = 10
+
+var PressPlan: String = "meeting," + Methods
+
+val CompilationCode: String = "full information"
+
+val StatsCollect: String = "partial information"
+
+val DetailedInfo: String = CompilationCode + StatsCollect
+
+var GetAllowStatus: Boolean = true
+
+var ExclusiveAllow: Boolean = true
+
+val PartnerList: String = SponsorList
+
+var Report: String = "photo, video and etc"
+
+var PrizePlan: String = "biggest prize for the first place and etc"
+
+private val EmergencyContacts: String = "911"
+
+var ExtraConditions: Boolean = true
+
+var FunLevel: Int = 100
+
+val TotalPlan: String = EventLog
+
+private val SpecialGuest: String = "Terry Crews"
+
+val MaximumCapacity: Int = 150
+
+var TeamTiming: Int = 12
