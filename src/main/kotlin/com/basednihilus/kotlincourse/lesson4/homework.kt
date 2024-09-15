936c16ef214022443c119f6fc1ced54439264d8b
BasedNihilus <davletmg@gmail.com>
2024-09-15 18:45:22 +0300

    homework is done

--- src/main/kotlin/com/basednihilus/kotlincourse/lesson4/homework.kt
+++ src/main/kotlin/com/basednihilus/kotlincourse/lesson4/homework.kt
@@ -1,42 +1,81 @@
 package com.basednihilus.kotlincourse.lesson4
 
-42 Byte
-98765432123456789L Long
-23.45f Float
-0.123456789 Double
-"Kotlin & Java" String
-false Boolean
-'c' Char
-500 Short
-4294967296L Long
-18.0f Float
--0.001 Double
-"OpenAI" String
-“true” Boolean
-'9' Char
-2048 Short
-10000000000L Long
-5.75f Float
-1.414 Double
-"Artificial Intelligence" String
-false Boolean
-'@' Char
-1024 Short
-1234567890123L Long
-10.01f Float
--273.15 Double
-"SpaceX" String
-true Boolean
-“🤯” String
-‘65535’ String
-72057594037927935L Long
-2.71828f Float
-101.0101 Double
-"Quantum Computing" String
-false Boolean
-'x' Char
-314  Short
-123456789123456789L Long
-6.626f Float
-0.007 Double
-"Android Studio" String
+val q: Byte = 42
+
+val w: Long = 98765432123456789L
+
+val e: Float = 23.45f
+
+val r: Double = 0.123456789
+
+val t: String = "Kotlin & Java"
+
+val y: Boolean = false
+
+val u: Char = 'c'
+
+val i: Short = 500
+
+val o: Long = 4294967296L
+
+val p: Float = 18.0f
+
+val a: Double = -0.001
+
+val s: String = "OpenAI"
+
+val d: String = “true”
+
+val f: Char = '9'
+
+val g: Short = 2048
+
+val h: Long = 10000000000L
+
+val j: Float = 5.75f
+
+val k: Double = 1.414
+
+val l: String = "Artificial Intelligence"
+
+val z: Boolean = false
+
+val x: Char = '@'
+
+val c: Short = 1024
+
+val v: Long = 1234567890123L
+
+val b: Float = 10.01f
+
+val n: Double = -273.15
+
+val m: String = "SpaceX"
+
+val qw: Boolean = true
+
+val we: String = “🤯”
+
+val er: String = ‘65535’
+
+val rt: Long = 72057594037927935L
+
+val ty: Float = 2.71828f
+
+val yu: Double = 101.0101
+
+val ui: String = "Quantum Computing"
+
+val io: Boolean = false
+
+val op: Char = 'x'
+
+val al: Short = 314
+
+val sd: Long = 123456789123456789L
+
+val df: Float = 6.626f
+
+val fg: Double = 0.007
+
+val gh: String = "Android Studio"
