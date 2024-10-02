75e45b9d59fab97c9437f41098966c593d5db627
BasedNihilus <davletmg@gmail.com>
2024-10-02 16:00:03 +0300

    homework is done

---
+++ src/main/kotlin/com/basednihilus/kotlincourse/lesson9/homework.kt
@@ -0,0 +1,9 @@
+package com.basednihilus.kotlincourse.lesson9
+
+fun main() {
+    HappyBirthday("Александр Молокович", 41)
+}
+
+fun HappyBirthday(name: String, age: Int) {
+    println("$name, поздравляю тебя с днем рождения! $age - прекрасный возраст, все только начинается! Желаю тебе всех благ!")
+}
