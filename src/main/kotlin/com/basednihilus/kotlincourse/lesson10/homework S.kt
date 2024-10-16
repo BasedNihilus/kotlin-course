26c07c5aec1c84c15491e377d6764501b86359c8
BasedNihilus <davletmg@gmail.com>
2024-10-16 18:45:24 +0300

    homework is done

--- src/main/kotlin/com/basednihilus/kotlincourse/lesson10/homework S.kt
+++ src/main/kotlin/com/basednihilus/kotlincourse/lesson10/homework S.kt
@@ -2,114 +2,84 @@ package com.basednihilus.kotlincourse.lesson10
 
 fun main() {
 
-    println("----задача 1----")
-    val set1 = setOf<Int>()
+    println("task1")
+    val set1 = setOf<Int> ()
     println(set1)
 
-
-    println("----задача 2----")
-    val set2 = setOf(1, 2, 3)
+    println("task2")
+    val set2 = setOf<Int> (1, 2, 3)
     println(set2)
 
-
-    println("----задача 3----")
-    val set3 = mutableSetOf("Kotlin", "Java", "Scala")
+    println("task3")
+    val set3 = mutableSetOf<String> ("Kotlin", "Java", "Scala")
     println(set3)
 
+    println("task4")
+    set3.add("Swift")
+    set3.add("Go")
+    println(set3)
 
-    println("----задача 4----")
-    val set4 = mutableSetOf("Kotlin", "Java", "Scala")
-    set4.add("Swift")
-    set4.add("Go")
-    println(set4)
-
-
-    println("----задача 5----")
-    val set5 = mutableSetOf(1, 2, 3, 4, 5, 6)
+    println("task5")
+    val set5 = mutableSetOf<Int> (1, 2, 3)
     set5.remove(2)
     println(set5)
 
-
-    println("----задача 6----")
-    val set6 = setOf(1, 2, 3, 4, 5, 6)
-    for (i in set6){
+    println("task6")
+    val set6 = setOf<Int> (1, 2, 3)
+    for (i in set6) {
         println(i)
     }
 
+    println("task7")
+    val stringSet = setOf<String> ("Hello", "World")
+    println(task7(stringSet, "Hell"))
 
-    println("----задача 7----")
-    val set7 = setOf("один", "два", "три", "четыре", "пять")
-    val searchString = "три"
-    println(search(set7, searchString))
-
-
-    println("----задача 8----")
-    val set8 = setOf("один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь")
-    val secondSet8 = setOf("два","четыре","шесть", "восемь", "стопицот", "дофигища")
-    val unitedSet8 = mutableSetOf<String>()
-    for (i in set8){
-        unitedSet8.add(i)
+    println("task8")
+    val set8 = setOf<String> ("Hello", "World", "Korlin")
+    val set81 = setOf<String> ("Hi", "Earth", "Java", "World")
+    var set82 = mutableSetOf<String>()
+    for (i in set8) {
+        set82.add(i)
     }
-    for (i in secondSet8){
-        unitedSet8.add(i)
+    for (i in set81) {
+        set82.add(i)
     }
-    println(unitedSet8)
+    println(set82)
 
-
-    println("----задача 9----")
-    val set9 = setOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    val secondSet9 = setOf(5, 12, 14, 4, 22, 7, 15, 33, 45, 2)
-    val commonSet9 = mutableSetOf<Int>()
-    for(i in set9) {
-        if (secondSet9.contains(i)){
-            commonSet9.add(i)
-        }
-    }
-    println(commonSet9)
-
-    println("----задача 9 вариант 2----")
+    println("task9")
+    val set9 = setOf<Int> (1, 2, 3, 4)
+    val set91 = setOf<Int> (4, 3, 6, 7)
     for (i in set9) {
-        for (j in secondSet9) {
-            if (i == j) {
-                commonSet9.add(i)
-            }
+        if (i in set91) {
+            println(i)
         }
     }
-    println(commonSet9)
 
-
-    println("----задача 10----")
-    val set10 = setOf("один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь")
-    val secondSet10 = setOf("два","четыре","шесть", "восемь", "стопицот", "дофигища")
-    val differenceSet10 = mutableSetOf<String>()
-    for (i in set10){
-        var inSecondSet = false
-        for (j in secondSet10){
-            if(i == j) {
-                inSecondSet = true
-            }
-        }
-        if (!inSecondSet){
-            differenceSet10.add(i)
+    println("task10")
+    val set10 = setOf<String> ("Kotlin", "Two", "Three", "Hello")
+    val set101 = setOf<String> ("Four", "Five", "Six", "Hello")
+    for (i in set10) {
+        if (i !in set101) {
+            println(i)
         }
     }
-    println(differenceSet10)
 
-
-    println("----задача 11----")
-    val set11 = setOf("один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь")
-    val list11 = mutableListOf<String>()
-    for (i in set11){
+    println("task11")
+    val set11 = setOf<String> ("Four", "Five", "Six", "Hello")
+    var list11 = mutableListOf<String> ()
+    for (i in set11) {
         list11.add(i)
-    }
+        }
     println(list11)
+
 }
 
-fun search(set7: Set<String>, searchString: String ): Boolean{
-    for (i in set7) {
-        if (i == searchString) {
-            return true
+fun task7(stringSet: Set<String>, stringString: String): Any {
+    var totalResult: Boolean = false
+    for (i in stringSet) {
+        if (i == stringString) {
+            totalResult = true
         }
     }
-    return false
-}
+    return totalResult
+    }
