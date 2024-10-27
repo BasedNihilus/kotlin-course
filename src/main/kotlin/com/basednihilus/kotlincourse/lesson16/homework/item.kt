cd540dee77bfb445181dbfa0cab18e428597cfab
BasedNihilus <davletmg@gmail.com>
2024-10-27 18:23:15 +0300

    homework is done

---
+++ src/main/kotlin/com/basednihilus/kotlincourse/lesson16/homework/item.kt
@@ -0,0 +1,7 @@
+package com.basednihilus.kotlincourse.lesson16.homework
+
+data class Item(
+    val name: String,
+    val type: String,
+    val weight: Int
+)
---
+++ src/main/kotlin/com/basednihilus/kotlincourse/lesson16/homework/itemType.kt
@@ -0,0 +1,16 @@
+package com.basednihilus.kotlincourse.lesson16.homework
+
+object ItemType {
+
+    const val LIQUIDS = "liquids"
+    const val ALCOHOL = "alcohol"
+    const val FRUIT = "fruit"
+    const val VEGETABLE = "vegetable"
+}
+
+val conversionFactors = mapOf(
+    ItemType.LIQUIDS to 0.95,
+    ItemType.ALCOHOL to 0.8,
+    ItemType.FRUIT to 0.65,
+    ItemType.VEGETABLE to 0.55
+)
